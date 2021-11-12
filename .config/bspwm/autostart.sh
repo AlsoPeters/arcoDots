263e465baea12adb26464749daf01c689c08bea2
AlsoPeters <kyle.fire@gmail.com>
2021-11-12 11:05:36 +0900

    added bspwm, polybar, rofi, tmux, zshrc config

---
+++ .config/bspwm/autostart.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+
+#Dual monitor with different resolution
+#xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x248 --rotate normal --output DisplayPort-1 --mode 3840x2160 --pos 2560x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off
+
+#Dual monitor with same resolution
+xrandr --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-1 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-A-0 --off --output HDMI-A-1 --off
+
+#xrandr --output DisplayPort-0 --primary --mode 2560x1440+0+0+0 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output DisplayPort-1 --mode 3840x2160 --output DP3 --right-of DisplayPort-0
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+#autorandr horizontal
+
+$HOME/.config/polybar/launch.sh &
+
+#change your keyboard if you need it
+#setxkbmap -layout be
+
+keybLayout=$(setxkbmap -v | awk -F "+" '/symbols/ {print $2}')
+
+if [ $keybLayout = "be" ]; then
+  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc-azerty &
+else
+  run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
+fi
+
+#Some ways to set your wallpaper besides variety or nitrogen
+#feh --bg-scale ~/.config/bspwm/wall.png &
+feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+#feh --randomize --bg-fill ~/Képek/*
+#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
+
+dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
+xsetroot -cursor_name left_ptr &
+
+# conky -c $HOME/.config/bspwm/system-overview &
+run variety &
+run nm-applet &
+run pamac-tray &
+run xfce4-power-manager &
+numlockx on &
+blueberry-tray &
+picom --config $HOME/.config/bspwm/picom.conf &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/notifyd/xfce4-notifyd &
+run volumeicon &
+#nitrogen --restore &
+#run caffeine &
+#run vivaldi-stable &
+#run firefox &
+#run thunar &
+#run dropbox &
+#run insync start &
+#run discord &
+#run spotify &
+#run atom &
